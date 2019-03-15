cab23dcd1433ef4ff712e09f77032579e09e4925
Q-WHai <return_zero0@163.com>
2019-03-15 18:27:10 +0800

    add aio

--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 ## simple-io
-> 针对Java中的 BIO/NIO/AIO 测试的程序
+
+[![lang](https://img.shields.io/badge/lang-java-brightgreen.svg)]()
+[![ide](https://img.shields.io/badge/ide-IntelliJ%20IDEA-brightgreen.svg)]()
+[![maven](https://img.shields.io/badge/maven-3.6.0-brightgreen.svg)]()
+
+> 针对Java中 BIO/NIO/AIO 的示例程序
---
+++ src/main/java/simple/io/aio/client/AIOClient.java
@@ -0,0 +1,75 @@
+package simple.io.aio.client;
+
+import simple.io.SocketConfig;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+
+public class AIOClient {
+
+    public static void main(String[] args) throws IOException {
+
+        final AsynchronousSocketChannel client = AsynchronousSocketChannel.open();
+
+        InetSocketAddress serverAddress = new InetSocketAddress(SocketConfig.DOMAIN, SocketConfig.PORT);
+
+        CompletionHandler<Void, ? super Object> handler = new CompletionHandler<Void, Object>() {
+
+            @Override
+            public void completed(Void result, Object attachment) {
+                client.write(ByteBuffer.wrap("Hello".getBytes()), null,
+                        new CompletionHandler<Integer, Object>() {
+
+                            @Override
+                            public void completed(Integer result,
+                                                  Object attachment) {
+                                final ByteBuffer buffer = ByteBuffer.allocate(1024);
+                                client.read(buffer, buffer, new CompletionHandler<Integer, ByteBuffer>() {
+
+                                    @Override
+                                    public void completed(Integer result,
+                                                          ByteBuffer attachment) {
+                                        buffer.flip();
+                                        System.out.println(new String(buffer.array()));
+                                        try {
+                                            client.close();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void failed(Throwable exc,
+                                                       ByteBuffer attachment) {
+                                    }
+
+                                });
+                            }
+
+                            @Override
+                            public void failed(Throwable exc, Object attachment) {
+                            }
+
+                        });
+            }
+
+            @Override
+            public void failed(Throwable exc, Object attachment) {
+                // TODO
+            }
+
+        };
+
+        client.connect(serverAddress, null, handler);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
